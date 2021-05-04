6115548ec352fd14dfb3b0157106c6dd04d87cac
AAKASH DEEP <48329027+aakashdeep983@users.noreply.github.com>
2021-05-04 15:38:39 +0530

    Initial commit

---
+++ app/src/main/java/com/aakashdeep/modification/filters/FilterListener.java
@@ -0,0 +1,7 @@
+package com.aakashdeep.modification.filters;
+
+import S.aakashdeep.photoeditor.PhotoFilter;
+
+public interface FilterListener {
+    void onFilterSelected(PhotoFilter photoFilter);
+}
---
+++ photoeditor/src/test/java/S/aakashdeep/photoeditor/EnumTest.java
@@ -0,0 +1,19 @@
+package S.aakashdeep.photoeditor;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EnumTest {
+
+    @Test
+    public void testNumberOfViewTypes() {
+        assertEquals(ViewType.values().length, 4);
+    }
+
+    @Test
+    public void testNumberOfPhotoFilterTypes() {
+        assertEquals(PhotoFilter.values().length, 24);
+    }
+
+}
